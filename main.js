0695402dcb4c30d9b6751b35264c1b090329f538
unknown <masalmanouran@gmail.com>
2024-06-06 21:23:59 +0300

    socond jsTasck

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-document.querySelector(".userProfile h2").textContent="hello my name is nouran";
-document.querySelector(".userInfo").classList.add("test");
+var parag=document.querySelector("p");
+var header=document.querySelector("h2");
+var bigBtn=document.querySelector(".bigger");
+var smallBtn=document.querySelector(".smaller");
+
+function FontSizeToBig()
+{
+parag.classList.add("bigFont");
+header.classList.add("bigFont");
+}
+
+
+function FontSizeToSmall()
+{
+parag.classList.add("smallFont");
+header.classList.add("smallFont");
+}
+bigBtn.onclick=FontSizeToBig;
+smallBtn.onclick=FontSizeToSmall;
+
